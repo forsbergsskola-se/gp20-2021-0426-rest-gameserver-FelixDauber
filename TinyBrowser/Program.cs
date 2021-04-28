1cc392f514635cf93815fa9a69b0223e20519a49
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2021-04-28 13:38:34 +0200

    before StreamReader

---
+++ TinyBrowser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TinyBrowser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new BrowserClient();
+            client.Start("acme.com", 0.9f);
+        }
+    }
+}
