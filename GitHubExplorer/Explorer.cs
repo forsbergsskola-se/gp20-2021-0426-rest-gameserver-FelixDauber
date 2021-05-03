67a32ca18ffda182cd8709a06584375145c53a1f
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2021-05-03 13:06:11 +0200

    prepares some things for the github explorer

---
+++ GitHubExplorer/Explorer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GitHubExplorer
+{
+    public class Explorer
+    {
+        //Github structure...
+        //Main page -> gitHub.com
+        //User name -> / + "user name"
+        //Project name -> / + "project name"
+        
+        
+        //What does this github explorer seek to do...
+        //Ask for a user name to explore
+
+        public string userName = "";
+
+        public void Start()
+        {
+            SelectUserFromInput();
+        }
+
+        #region User Related
+        public void SelectUserFromInput()
+        {
+            Console.WriteLine("Select user");
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (AttemptUserConnection(input)) break;
+            }
+        }
+        
+        public bool AttemptUserConnection(string userName)
+        {
+            //Checks if the user is a viable pick
+            //If the user is viable -> Assign user as the new user and return true
+            //Else -> return false & throw error message
+            return false;
+        }
+        #endregion
+    }
+}
---
+++ GitHubExplorer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitHubExplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var explorer = new Explorer();
+            explorer.Start();
+        }
+    }
+}
